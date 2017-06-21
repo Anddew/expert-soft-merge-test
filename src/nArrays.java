1282601eb7a104c28e4f520dba6116ea91ddd44c
Andrey Bylinovich <anddew90@gmail.com>
2017-06-21 20:49:23 +0300

    Some fixes.

--- src/nArrays.java
+++ src/nArrays.java
@@ -1,19 +1,20 @@
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 
 public class nArrays {
     public static void main(String[] args) {
-        ArrayList<Integer> a = createIntegerList();
-        ArrayList<Integer> b = createIntegerList();
+        List<Integer> a = createIntegerList();
+        List<Integer> b = createIntegerList();
         merge(a, b);
-        System.out.println("First array: " + Arrays.toString(a.toArray()));
-        System.out.println("Second array: " + Arrays.toString(b.toArray()));
+        System.out.println("First array: " + a);
+        System.out.println("Second array: " + b);
+
     }
 
-    public static void merge(ArrayList<Integer> a, ArrayList<Integer> b) {
-        ArrayList<Integer> newArray = new ArrayList<>(a.size());
+    public static void merge(List<Integer> a, List<Integer> b) {
+        List<Integer> newArray = new ArrayList<>(a.size());
         newArray.addAll(a);
         int resultSize = a.size() + b.size();
         a.clear();
@@ -33,8 +34,8 @@ public class nArrays {
         }
     }
 
-    private static ArrayList<Integer> createIntegerList() {
-        ArrayList<Integer> result = new ArrayList<>();
+    private static List<Integer> createIntegerList() {
+        List<Integer> result = new ArrayList<>();
         Random random = new Random();
         int minSize = 3;
         int value = 0;
@@ -42,7 +43,7 @@ public class nArrays {
             value += random.nextInt(10);
             result.add(value);
         }
-        System.out.println("List created. Size = " + result.size() + ". List: " + Arrays.toString(result.toArray()) + '\n');
+        System.out.println("List created. Size = " + result.size() + ". List: " + result + '\n');
         return result;
     }
 
